5908e493cad6046ff60a9c43edaaf26e4f969d07
user <ivan.isaev7@icloud.com>
2025-07-23 14:56:20 +0300

    + success order

--- src/core/views.py
+++ src/core/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import OrderForm
+from .utils import notify_telegram_new_order
 
 def order_create(request):
     if request.method == "POST":
         form = OrderForm(request.POST)
         if form.is_valid():
-            form.save()
+            order = form.save()                        # Сохраняем в переменную!
+            notify_telegram_new_order(order)           # Уведомление в TG
             return render(request, "core/order_success.html")
     else:
         form = OrderForm()
     return render(request, "core/order_form.html", {"form": form})
-
 
 def index(request):
     return render(request, "core/index.html")
